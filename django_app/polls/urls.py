b966a293402c9e05bdaf84650af866e8149db0d6
hanabee337 <hanabee337@gmail.com>
2017-02-06 19:53:36 +0900

    index 뷰에서 템플릿 뷰 출력(?)
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/polls/urls.py
+++ django_app/polls/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # /polls/
     url(r'^$', views.index),
     # /pols/5
-    url(r'^(?P<question_id>[0-9])+/S', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9])+/$', views.detail, name='detail'),
     # /pols/5/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # /pols/5/vote/
--- django_app/polls/views.py
+++ django_app/polls/views.py
@@ -1,22 +1,30 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Question
 
 
 def index(request):
+
     # pub_date 필드를 기준으로 내림차순 정렬한 결과를 latest_question_list에 할당
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
 
-    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # shorcut을 이용해 잛게 처리
+    return render(request, 'polls/index.html', context)
+
+    #template = loader.get_template('polls/index.html')
     # 템플릿 로더를 이용해서 'polls/index.html'에서 템플릿 파일을 가져온다
 
     # 템플릿 파일에 전달할 context 객체를 정의
-    context = {
+    #context = {
         # 'latest_question_list'라는 키에 값을 할당, 할당 키로 템플릿에서 사용가능하다.
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    #    'latest_question_list': latest_question_list,
+    #}
+    # 템플릿에 context와 request객체를 사용해서
+    #return HttpResponse(template.render(context, request))
 
 #    output = ','.join([q.question_text for q in latest_question_list])
 #    output2 = ''
