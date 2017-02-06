4d990aabcac3b7f175bacba3bf62f3b0c37761c0
hanabee337 <hanabee337@gmail.com>
2017-02-06 16:09:49 +0900

    Writing Django-App polls : part2
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/polls/models.py
+++ django_app/polls/models.py
@@ -1,4 +1,28 @@
+import datetime
+
 from django.db import models
 
+from django.utils import timezone
+
 # Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        '''
+        최근에 발생된 Question 메소드인지 판단해주는 메서드
+        :return: Boolean
+        '''
+        # 자신의 발생일자 >= 현재시각 - 하루만큼의 시간
+        return self.pub_date >= timezone.now()-datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    c_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.c_text
 
