e5d53ae00a0b8f0cb8fbf6836c5b274fc8a6b961
hanabee337 <hanabee337@gmail.com>
2017-02-06 16:20:00 +0900

    add polls detail, results & vote view, url
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/polls/urls.py
+++ django_app/polls/urls.py
@@ -2,5 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index)
+    # /polls/
+    url(r'^$', views.index),
+    # /pols/5
+    url(r'^(?P<question_id>[0-9])+/S', views.detail, name='detail'),
+    # /pols/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # /pols/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
